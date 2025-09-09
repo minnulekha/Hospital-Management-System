29ad2dd1ad5736a487208abc975d6945ce27f5ae
minnulekha <minnulekhavg@gmail.com>
2025-09-09 18:42:09 +0530

    Changed the satic files in template.

--- project/settings.py
+++ project/settings.py
@@ -7,7 +7,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY
 SECRET_KEY = os.environ.get("SECRET_KEY", "fallback-secret-key")
 DEBUG = os.environ.get("DEBUG", "False") == "True"
-ALLOWED_HOSTS = ["hospital-management-system-obzb.onrender.com"]
+ALLOWED_HOSTS = [
+    "hospital-management-system-obzb.onrender.com",
+    "localhost",
+    "127.0.0.1"
+]
+
 
 # Applications
 INSTALLED_APPS = [
